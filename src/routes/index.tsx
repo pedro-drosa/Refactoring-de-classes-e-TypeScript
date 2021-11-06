c3aec1bc5ca546c472312e1460de0fcab9cdc3f9
pedro-drosa <pedro_drosa@hotmail.com>
2021-11-06 07:11:35 -0300

    src/services/api.ts, src/routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 
-const Routes = () => (
+const Routes:React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
   </Switch>
