2e533f3d377ad88dcac829a9559a88a8ec6eb460
pedro-drosa <pedro_drosa@hotmail.com>
2021-11-08 08:35:52 -0300

    Header e Input

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import { FiPlusSquare } from 'react-icons/fi';
+import { Container } from './styles';
+
+const Logo = require("../../assets/logo.svg") as string;
+
+interface HeaderProps {
+  openModal: () => void
+}
+
+const Header = ({ openModal }:HeaderProps) => {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+}
+
+export default Header;
